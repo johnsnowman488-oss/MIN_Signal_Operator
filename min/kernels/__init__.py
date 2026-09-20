ef90b86c74537d288572701d0d57ac229ae04af5
johnsnowman488-oss <johnsnowman488@gmail.com>
2026-09-20 22:52:49 +0000

    feat: expose Prony and SOE interfaces

--- min/kernels/__init__.py
+++ min/kernels/__init__.py
@@ -1,5 +1,7 @@
-"""Kernel functions for MIN experiments."""
+"""Kernel functions and SOE identification for MIN experiments."""
 
 from .exponential import exponential_kernel
+from .prony import PronySOE, fit_prony
+from .soe import SOEMemory
 
-__all__ = ["exponential_kernel"]
+__all__ = ["exponential_kernel", "PronySOE", "SOEMemory", "fit_prony"]
