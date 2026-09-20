b097a55493daedbaf75e21c33feaa721717facc2
johnsnowman488-oss <johnsnowman488@gmail.com>
2026-09-20 23:14:51 +0000

    feat: expose power-law kernel

--- min/kernels/__init__.py
+++ min/kernels/__init__.py
@@ -1,7 +1,14 @@
 """Kernel functions and SOE identification for MIN experiments."""
 
 from .exponential import exponential_kernel
+from .power_law import power_law_kernel
 from .prony import PronySOE, fit_prony
 from .soe import SOEMemory
 
-__all__ = ["exponential_kernel", "PronySOE", "SOEMemory", "fit_prony"]
+__all__ = [
+    "exponential_kernel",
+    "power_law_kernel",
+    "PronySOE",
+    "SOEMemory",
+    "fit_prony",
+]
