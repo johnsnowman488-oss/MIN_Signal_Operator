a0a4fb5300af2f2797152f91a2cef9836bd8c073
johnsnowman488-oss <johnsnowman488@gmail.com>
2026-09-20 23:14:42 +0000

    fix: export Prony and SOE components

--- min/__init__.py
+++ min/__init__.py
@@ -2,5 +2,15 @@
 
 from .core.operator import MemoryOperator
 from .kernels.exponential import exponential_kernel
+from .kernels.power_law import power_law_kernel
+from .kernels.prony import PronySOE, fit_prony
+from .kernels.soe import SOEMemory
 
-__all__ = ["MemoryOperator", "exponential_kernel"]
+__all__ = [
+    "MemoryOperator",
+    "exponential_kernel",
+    "power_law_kernel",
+    "PronySOE",
+    "SOEMemory",
+    "fit_prony",
+]
