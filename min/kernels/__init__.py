ce07754892e81315bff66fd73e754bcb1d754be5
johnsnowman488-oss <johnsnowman488@gmail.com>
2026-09-21 04:17:48 +0000

    Update min/kernels/__init__.py for Experiment 05

--- min/kernels/__init__.py
+++ min/kernels/__init__.py
@@ -4,6 +4,9 @@ from .exponential import exponential_kernel
 from .power_law import power_law_kernel
 from .prony import PronySOE, fit_prony
 from .soe import SOEMemory
+from .nnls import NNLSSOE, fit_nnls, log_rate_grid
+from .subspace import SubspaceSOE, fit_esprit, fit_matrix_pencil
+from .vector_fit import VectorFitResult, fit_vector_fitting
 
 __all__ = [
     "exponential_kernel",
@@ -11,4 +14,12 @@ __all__ = [
     "PronySOE",
     "SOEMemory",
     "fit_prony",
+    "NNLSSOE",
+    "fit_nnls",
+    "log_rate_grid",
+    "SubspaceSOE",
+    "fit_esprit",
+    "fit_matrix_pencil",
+    "VectorFitResult",
+    "fit_vector_fitting",
 ]
