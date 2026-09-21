d669f58e444b64dde590d1d0477d384fed9c30da
johnsnowman488-oss <johnsnowman488@gmail.com>
2026-09-21 23:04:46 +0000

    Export receiver compensation metrics

--- min/metrics/__init__.py
+++ min/metrics/__init__.py
@@ -17,19 +17,20 @@ from .frequency import (
     finite_horizon_kernel_response,
     group_delay,
 )
+from .equalization import (
+    add_awgn,
+    apply_fir_equalizer,
+    apply_scalar_compensation,
+    complex_gain_fit,
+    design_fir_equalizer,
+    evm,
+)
 
 __all__ = [
-    "energy",
-    "rms",
-    "peak_magnitude",
-    "mse",
-    "nmse",
-    "correlation",
-    "delay_of_max_correlation",
-    "complex_tone",
-    "estimate_complex_gain",
-    "exponential_frequency_response",
-    "soe_frequency_response",
-    "finite_horizon_kernel_response",
-    "group_delay",
+    "energy", "rms", "peak_magnitude", "mse", "nmse", "correlation",
+    "delay_of_max_correlation", "complex_tone", "estimate_complex_gain",
+    "exponential_frequency_response", "soe_frequency_response",
+    "finite_horizon_kernel_response", "group_delay", "add_awgn",
+    "apply_fir_equalizer", "apply_scalar_compensation", "complex_gain_fit",
+    "design_fir_equalizer", "evm",
 ]
